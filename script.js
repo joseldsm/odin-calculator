b2bc27f2f37ff6e97f823b04cccbc1420761b295
joseldsm <165505426+joseldsm@users.noreply.github.com>
2024-09-17 15:37:50 +0200

    Adding events for number buttons, operator buttons, AC button and = button. Adding function to store values, rearrange them and calculate the given operation. Works but need to improve.

--- script.js
+++ script.js
@@ -28,27 +28,79 @@ buttonsArray.sort((a, b) => a.position - b.position)
 });
 
 //OPERATORS CALCULATION
-function operate(operator, num1, num2) {
+function operate(operator, a, b) {
     switch (operator) {
-        case "add":
-            return num1 + num2;
-        case "subtract":
-            return num1 - num2;
-        case "multiply":
-            return num1 * num2;
-        case "divide":
-            return num1 / num2;
+        case "+":
+            return a + b;
+        case "-":
+            return a - b;
+        case "\u00D7":
+            return a * b;
+        case "%":
+            return a / b;
         default:
             return "not a valid operator";
       }
 }
 
-//DISPLAY
-let buttonsSelector =  document.querySelectorAll("button");
+let buttonsNumber =  document.querySelectorAll(".number");
+let buttonsOperator = document.querySelectorAll(".operator");
+let buttonResult = document.querySelector(".result");
+let buttonClear = document.querySelector(".clear");
 let display = document.querySelector("#display");
+let calculation = [];
 
-buttonsSelector.forEach((button) => {
+//CLICK NUMBERS
+buttonsNumber.forEach((button) => {
         button.addEventListener("click", () => {
             display.textContent += button.textContent;
+            calculation.push(Number(button.textContent));
 })
 });
+
+//CLICK OPERATORS
+buttonsOperator.forEach((button) => {
+    button.addEventListener("click", () => {
+        display.textContent += button.textContent;
+        calculation.push(button.textContent);
+})
+});
+
+//CLICK =
+buttonResult.addEventListener("click", () => {
+    console.table(calculation);
+    let calculationCleaned = cleaningCalc(calculation);
+    console.table(calculationCleaned);
+    let result =  operate(calculationCleaned[1], calculationCleaned[0], calculationCleaned[2]);
+    display.textContent = result;
+    calculation = [result];
+})
+
+//CLICK AC
+buttonClear.addEventListener("click", () => {
+    display.textContent = "";
+    calculation = [];
+})
+
+//CLEAN ARRAY : GATHER NUMBERS, CHECK IF FORMAT IS OK.
+function cleaningCalc(array) {
+    let operatorDetector = 0;
+    let numberDetector = 0;
+    let cleanedArray = ["","",""];
+
+    for (let i = 0; i < array.length; i++) {
+        if (typeof(array[i]) === 'number' && operatorDetector === 0) {
+            cleanedArray[0] += String(array[i]);
+            numberDetector = 1;
+        } else if (typeof(array[i]) !== 'number' && numberDetector === 1) {
+            cleanedArray[1] = (array[i]);
+            operatorDetector++;
+        } else if (typeof(array[i]) === 'number' && operatorDetector === 1) {
+            cleanedArray[2] += String(array[i]);
+        } else {
+            alert("error");
+            break;
+        }
+    }
+    return cleanedArray;
+}
